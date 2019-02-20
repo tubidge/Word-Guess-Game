b8ac3220619631151a1e24eea7048661bea5a66e
tubidge <tubidge@gmail.com>
2019-02-19 21:50:59 -0700

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// Array of ______ for computer to choose from
+var computerChoices = []
+
+// Variables to hold number of wins, losses, guesses left, and letters guessed
+var currentWord = undefined;
+var 
