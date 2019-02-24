39a687817c8e1653232d37ea1485f544b425c7fe
tubidge <tubidge@gmail.com>
2019-02-24 14:01:15 -0700

    working underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,9 @@
 var computerChoices = ["one", "two", "three", "four", "five"];
 
 // Variables to hold number of wins, losses, guesses left, and letters guessed
-var currentWord = undefined;
 var guessesRemaining = 10;
 var lettersGuessed  = [];
+var underscores = [];
 var wins = 0;
 var losses = 0;
 // Variables holding reference to HTML elements
@@ -17,6 +17,22 @@ var lossesText = document.getElementById("losses");
 // Variable to choose computer guess on page load
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 console.log("Computer Guess: " + computerGuess);
+lettersInComputerGuess = computerGuess.split("");
+
+// Display underscores in place of chosen word
+for (var i = 0; i < computerGuess.length; i++) {
+    console.log(i);
+    underscores.push("_");
+};
+console.log(underscores);
+
+document.getElementById("current-word").innerHTML = underscores.join(" ");
+
+
+// Variable to hold the length of the computerGuess
+// var computerGuessLength = computerGuess.length;
+// console.log("Length of computer guess :" + computerGuessLength);
+
 
 // User's guess is captured and made lower case
 document.onkeyup = function() {
@@ -26,9 +42,19 @@ document.onkeyup = function() {
     lettersGuessed.push(" " + userGuess);
 // Update new lettersGuessed to page
     lettersGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
+    
 
 // Check if userGuess is correct
 // If so, display letter in correct location in currentWord
 
+for ( var i = 0; i < lettersInComputerGuess.length; i++) {
+    if (userGuess === lettersInComputerGuess[i]) {
+        console.log("true");
+        underscores[i] = userGuess;
+        // This replaces the entire current-word element with the one correct guessed letter
+        // I want to replace just the one letter in the correcet location
+        document.getElementById("current-word").innerHTML = underscores[i];
 
+    };
+};
 };
